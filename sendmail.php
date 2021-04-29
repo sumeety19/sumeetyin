d2b3359d3e083291402b7e972f51bce453afe4c9
sumeety19 <68916452+sumeety19@users.noreply.github.com>
2021-04-30 01:40:11 +0530

    PHP Error

--- sendmail.php
+++ sendmail.php
@@ -1,11 +1,11 @@
 <?php
 //get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$subject= $_POST['subject'];
-$message= $_POST['message'];
-$to = "youremail@mail.com";
-$subject = "sumeetban19@gmail.com";
+$name = $_POST['Name'];
+$email= $_POST['Email'];
+$subject= $_POST['Subject'];
+$message= $_POST['Message'];
+$to = "sumeetban19@gmail.com";
+$subject = "Mail From sumeety.in";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Subject = " . $subject . "\r\n Message =" . $message;
 $headers = "From: noreply@sumeety.in" . "\r\n" .
 "CC: somebodyelse@example.com";
